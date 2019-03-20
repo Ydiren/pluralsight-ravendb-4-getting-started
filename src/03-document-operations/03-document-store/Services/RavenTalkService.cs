3ceba7eedd47f89c94fdf84e32a73258c2cad60e
Andy Green <Andy.Green@datapath.co.uk>
2019-03-20 12:23:27 +0000

    - Create new Talk entity - Retrieve talk entity by Id - Implemented GetAllSpeakers

--- src/03-document-operations/03-document-store/Services/RavenTalkService.cs
+++ src/03-document-operations/03-document-store/Services/RavenTalkService.cs
@@ -17,21 +17,66 @@ namespace Sample.Services
         {
             this.store = storeHolder.Store;
         }
-        
+
         public async Task<Talk> CreateTalk(NewTalk talk)
         {
-            throw new NotImplementedException("TODO: Implement CreateTalk");
+            using (var session = store.OpenAsyncSession())
+            {
+                var newTalk = new Talk()
+                {
+                    Headline = talk.Headline,
+                    Description = talk.Description,
+                    Speaker = talk.Speaker
+                };
+
+                // Store new talk in the cache
+                // This will populate the ID of the object passed in
+                await session.StoreAsync(newTalk);
+
+                // Commit changes to the actual database
+                await session.SaveChangesAsync();
+
+                return newTalk;
+            }
         }
 
         public async Task<TalkDetail> GetTalkDetail(string id)
         {
-            throw new NotImplementedException("TODO: Implement GetTalkDetail");
+            using (var session = store.OpenAsyncSession())
+            {
+                // Include will load the referenced speaker into the cache, ready for the next Load operation
+                var talk = await session.Include<Talk>(t => t.Speaker)
+                                        .LoadAsync<Talk>(id);
+
+                // This will just fetch the speaker out of the cache, it will
+                // not hit the database due to the Include in the previous statement
+                var speaker = await session.LoadAsync<Speaker>(talk.Speaker);
+
+                return new TalkDetail()
+                {
+                    Id = talk.Id,
+                    Description = talk.Description,
+                    Event = talk.Event,
+                    Headline = talk.Headline,
+                    Published = talk.Published,
+                    Speaker = talk.Speaker,
+                    SpeakerName = speaker.Name,
+                    SpeakerTalks = new TalkSummary[] { },
+                    Tags = talk.Tags
+                };
+            }
         }
-        
-        public async Task<Speaker[]> GetSpeakers() {
-            throw new NotImplementedException("TODO: Implement GetSpeakers");
+
+        public async Task<Speaker[]> GetSpeakers()
+        {
+            using (var session = store.OpenAsyncSession())
+            {
+                // Load speakers with keys that are prefixed with "Speaker/" (i.e. all of them!)
+                var speakers = await session.Advanced.LoadStartingWithAsync<Speaker>("Speakers/", start: 0, pageSize: Constants.PageSize);
+                return speakers.ToArray();
+            }
         }
-        
+
         public async Task<Talk> UpdateTalk(string id, UpdatedTalk talk, string version)
         {
             throw new NotImplementedException("TODO: Implement UpdateTalk");
@@ -42,7 +87,8 @@ namespace Sample.Services
             throw new NotImplementedException("TODO: Implement DeleteTalk");
         }
 
-        public async Task<SpeakerTalkStats[]> GetSpeakerTalkStats() {
+        public async Task<SpeakerTalkStats[]> GetSpeakerTalkStats()
+        {
             throw new NotImplementedException("TODO: Implement GetSpeakerTalkStats");
         }
 
